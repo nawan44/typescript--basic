5f147c9647399319c46adf2779c8e06d8d903cf1
nawan44 <rachmat.d.gunawan.com>
2020-10-18 05:12:50 +0700

    data type

---
+++ typescript--basic/data_type.ts
@@ -0,0 +1,8 @@
+let message1: string = "Rachmat Gunawan"
+let phoneNumber: number | string | boolean | null
+let age: number = 30
+let isActive: boolean = true
+
+console.log(typeof message1, message1)
+console.log(typeof age, age)
+console.log(typeof isActive, isActive)
