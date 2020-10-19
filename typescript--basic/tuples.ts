225cea873f374901dba41cf514fbceaf250cfce0
nawan44 <rachmat.d.gunawan.com>
2020-10-19 08:09:44 +0700

    tuples

---
+++ typescript--basic/tuples.ts
@@ -0,0 +1,8 @@
+const student: [string, number, boolean] = ['Rachmat', 2020, true]
+
+console.log(student[0])
+console.log(student[1])
+console.log(student[2])
+
+student[0] = 'Gunawan'
+console.log(student[0])
