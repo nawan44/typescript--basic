2be91a2ac463a15d4fdda616286f8a74029a8602
nawan44 <rachmat.d.gunawan.com>
2020-11-02 00:00:30 +0700

    switch

---
+++ conditionalSwtitch.ts
@@ -0,0 +1,17 @@
+const day = 5
+// const day = (new Date).getDay()
+
+switch (day) {
+    case 1:
+        console.log('Senin')
+        break;
+    case 3:
+        console.log('Rabu')
+        break;
+    case 5:
+        console.log('Jumat')
+        break;
+    default:
+        console.log('Tidak beroperasi')
+        break;
+}
