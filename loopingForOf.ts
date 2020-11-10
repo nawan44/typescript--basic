0fac7f94f439ff6e2dccd820c525184806a98d8a
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-11-10 16:34:13 +0700

    looping for of

---
+++ loopingForOf.ts
@@ -0,0 +1,23 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+const numbers = [10, 20, 30, 40]
+
+for (let value of numbers) {
+    console.log(value) // 10, 20, 30, 40
+}
+
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+for (let i = 0; i < days.length; i++) {
+    console.log(days[i])
+}
+for (let day of days) {
+    console.log(day)
+}
+
+
+
+
